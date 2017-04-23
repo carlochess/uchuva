b8c9426616c289a7cf12da16ded017db00ef22d0
Carlos Roman <carlochess@hotmail.com>
2017-04-23 00:39:11 -0500

    Deleted vagrantDO in flavor of terraform

--- vagrant/digitalocean/installscript.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#set -e
-# Directory in which PuppetFile is placed to be scanned by librarian-puppet.
-PUPPET_DIR=/vagrant/puppet/production
-sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-yum install -y git ruby puppet
-#gem install librarian-puppet
-#echo "Executing PuppetFile.."
-#cd $PUPPET_DIR && librarian-puppet install --no-use-v1-api --path modules
