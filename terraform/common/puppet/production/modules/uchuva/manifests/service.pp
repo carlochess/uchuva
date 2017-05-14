a13002159fca541ef5960987489b296a3a72d226
Carlos Roman <carlochess@hotmail.com>
2017-05-14 16:04:46 -0500

    Submissions to load managers allowed again

--- terraform/common/puppet/production/modules/uchuva/manifests/service.pp
+++ terraform/common/puppet/production/modules/uchuva/manifests/service.pp
@@ -2,14 +2,14 @@ class uchuva::service {
   include '::uchuva'
   $uchuvadir = $uchuva::uchuvadir
     exec {'UchuStart':
-        command => "/bin/bash -c 'source /etc/profile ; source ambiente ; pm2 start main.js --name=\"uchuva\"'",
+        command => "/bin/bash -c 'source /etc/profile ; source ambiente ; pm2 start main.js --name=uchuva'",
         path => ['/usr/bin', '/usr/sbin', '/bin'],
         cwd => "$uchuvadir/uchuva/prototipo",
         user => 'uchuva',
         onlyif => 'pm2 id uchuva | grep "\[\]"',
         environment => ["HOME=/home/uchuva"],
    }
-   
+
    exec {'UchuReStart':
         command => "/bin/bash -c 'source /etc/profile ; source ambiente ; pm2 restart --update-env uchuva'",
         path => ['/usr/bin', '/usr/sbin', '/bin'],
